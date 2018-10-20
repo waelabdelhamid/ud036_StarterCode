3d8f1767a09b74af5f32ec68d6f74043696be0d8
waelabdelhamid <wael.abdelhamid@gmail.com>
2018-10-20 12:42:23 -0700

    Update media.py

--- media.py
+++ media.py
@@ -8,7 +8,7 @@ class Movie():
     """
 
     def __init__(self, movie_title, poster_image, trailer_youtube):
-        
+
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
